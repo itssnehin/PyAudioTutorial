be5b58f895bc4513d504d1ddecf87e2a643146ba
snehin <snehin.kukreja@gmail.com>
2021-08-20 23:44:02 +0200

    Jupyter Notebook Audio interfacing attempt

--- soundtut.py
+++ soundtut.py
@@ -2,3 +2,17 @@ import pyaudio
 import numpy
 import matplotlib
 import wave
+import thread
+import time
+import subprocess
+
+
+#constants
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = 'tmp/tmp.wav'
+
+pa = pyaudio.PyAudio() # create a PyAudio instance
